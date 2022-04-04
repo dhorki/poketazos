ce97bca865ea0e6cb3bf34d6b6c20364f6c94776
Victor Infante <victor.infante@wizeline.com>
2022-04-03 19:26:31 -0500

    added type tooltip

--- src/components/TypeBadge.js
+++ src/components/TypeBadge.js
@@ -1,5 +1,12 @@
 const TypeBadge = ({ type }) => {
-  return <div className={`badge-bg  ${type}-badge-bg`} />;
+  return (
+    <div
+      className={`badge-bg  ${type}-badge-bg`}
+      data-toggle="tooltip"
+      data-placement="top"
+      title={type}
+    />
+  );
 };
 
 export default TypeBadge;
